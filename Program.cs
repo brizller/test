ef1753bd071b0210c05e2ec9cd3bf5a5abe8f884
Danileico Egor <egor.danileico@amdaris.com>
2022-07-29 15:09:17 +0300

    some modifications

--- Program.cs
+++ Program.cs
@@ -92,7 +92,7 @@ namespace New_Test
             
            Console.WriteLine(GetUniqueNumber(new int[] { 4, 10, 5, 4, 2, 10 }));
             Class.sayHi();
-            Console.Write("hi");
+            Console.Write("hello this world");
             Class.sayHi();
             Console.WriteLine("Hello World");// тестирование новой функции
             
